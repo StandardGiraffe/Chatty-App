79aa384cec282799f3b96fb6098e1e9e05d84632
StandardGiraffe <dannyfekete@gmail.com>
2018-10-02 00:48:15 +0000

    Initial commit: separated initial static HTML page into .jsx modules, linked everything up.  It's renderingggggg!

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,48 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import NavBar from './NavBar.jsx';
 
+// Parent component
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <NavBar />
+        <Messages />
+        <ChatBar />
+      </div>
     );
   }
 }
+
+class Messages extends Component {
+  render() {
+
+    return (
+      <main className="messages">
+        <div className="message">
+          <span className="message-username">Anonymous1</span>
+          <span className="message-content">I won't be impressed with technology until I can download food.</span>
+        </div>
+        <div className="message system">
+          Anonymous1 changed their name to nomnom.
+        </div>
+      </main>
+    );
+  }
+}
+
+// class ChatBar extends Component {
+//   render() {
+
+//     return (
+//       <footer className="chatbar">
+//         <input className="chatbar-username" placeholder="Your Name (Optional)" />
+//         <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+//       </footer>
+//     )
+//   }
+// }
+
 export default App;
+
