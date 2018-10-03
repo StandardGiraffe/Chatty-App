09d8c2e676baafd1652ed14619733cee881f6f91
StandardGiraffe <dannyfekete@gmail.com>
2018-10-03 20:22:17 +0000

    Moved UUID implementation to the server from the application.  The app now sends its message to the server, where it's parsed, id-stamped, and then broadcast back to all connections.  The connections listen for broadcasts and update the messages state.

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,7 +1,9 @@
 // server.js
 
 const express = require('express');
-const SocketServer = require('ws').Server;
+const WebSocket = require('ws');
+const uuidv4 = require('uuid/v4');
+
 
 // Set the port to 3001
 const PORT = 3001;
@@ -13,13 +15,28 @@ const server = express()
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
 // Create the WebSockets server
-const wss = new SocketServer({ server });
+const wss = new WebSocket.Server({ server });
 
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
   console.log('Client connected');
+
+  ws.on('message', (data) => {
+    // console.log(data);
+    const dataObject = JSON.parse(data);
+    dataObject.id = uuidv4();
+
+    // Now that the random ID has been tacked on, send it out to all connected clients in a stringified version.
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(dataObject));
+      }
+    });
+
+    console.log(`${dataObject.username} says: "${dataObject.content}".  Unique ID is ${dataObject.id}.`);
+  });
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => console.log('Client disconnected'));
--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import ChatBar from './ChatBar.jsx';
 import NavBar from './NavBar.jsx';
 import Messages from './Messages.jsx';
-import uuidv4 from 'uuid/v4';
 
 // Parent component
 class App extends Component {
@@ -11,18 +10,7 @@ class App extends Component {
 
     this.state = {
       currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-      messages: [
-        {
-          username: "Bob",
-          content: "Has anyone seen my marbles?",
-          id: uuidv4()
-        },
-        {
-          username: "Anonymous",
-          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
-          id: uuidv4()
-        }
-      ]
+      messages: []
     }
   }
 
@@ -31,26 +19,35 @@ class App extends Component {
     const newMessage = {
       username: messagePackage.username,
       content: messagePackage.content,
-      type: "message",
-      id: uuidv4()
+      type: "message"
     }
     // concat the message into the existing messages, inside the state
-    const messages = this.state.messages.concat(newMessage);
-    this.setState({ messages: messages }); // Update the message list
+
+    // Publishes messages to the server.  (USE SOON)
+    this.socketToMe.send(JSON.stringify(newMessage));
+
+
   }
 
+
   componentDidMount() {
-    // console.log("componentDidMount <App />");
-    // setTimeout(() => {
-    //   console.log("Simulating incoming message");
-    //   // Add a new message to the list of messages in the data store
-    //   const newMessage = {id: uuidv4(), username: "Michelle", content: "Hello there!"};
-    //   const messages = this.state.messages.concat(newMessage);
-    //   // Update the state of the app component.
-    //   // Calling setState will trigger a call to render() in App and all child components.
-    //   this.setState({messages: messages})
-    // }, 3000);
+    this.socketToMe = new WebSocket("ws://localhost:3001");
+
+    this.socketToMe.onopen = function (event) {
+      console.log("Hello!  You done connectified.");
+    }
+
+    this.socketToMe.onmessage = (event) => {
+
+      const newMessage = JSON.parse(event.data);
+      // console.log(newMessage);
+      const updatedMessages = this.state.messages.concat(newMessage);
+      this.setState({ messages: updatedMessages }); // Update the message list
+      // console.log(event.data);
+    }
+
   }
+
 
   render() {
     return (
